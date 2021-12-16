08359e1ce9a30713c5615ec3d7ffe9a3836560f0
elangovanven <elangovanven@gmail.com>
2021-12-16 19:16:08 +0530

    Very first commit

---
+++ operation.java
@@ -0,0 +1,20 @@
+
+public class operations {
+	
+	private void addition() {
+		
+		System.out.println("Sum of numbers");		
+		int a = 6;
+		int b = 7;		
+		int c = a+b;		
+		System.out.println("Sum of numbers is : "+c);
+	}
+	public static void main(String[] args) {
+		
+		operations o = new operations();
+		o.addition();
+		
+		
+	}
+
+}
